9c5a8c9a7284c4275ac4123b4552e656f9866cbb
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-02-28 16:35:06 -0300

    [Add] script para compilar codigo

---
+++ avaliacao-2021-02-28/compile.sh
@@ -0,0 +1,6 @@
+javac atividade/Contato.java -Xlint:unchecked
+javac atividade/ContatoTableModel.java -Xlint:unchecked
+javac atividade/Conexao.java -Xlint:unchecked
+javac atividade/MeuBotao.java -Xlint:unchecked
+javac atividade/InterfaceGrafica.java -Xlint:unchecked
+javac atividade/MainActivity.java -Xlint:unchecked
